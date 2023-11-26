8ba16a5a7b2808cd559d2fa13c2353f533aab5c4
Denis <denis2000.kuzik@gmail.com>
2023-11-26 14:07:42 +0300

    gtest integration wip

--- libs/ml-lib/exampleservice.h
+++ libs/ml-lib/exampleservice.h
@@ -3,17 +3,15 @@
 
 #include <QObject>
 
-class ExampleService : public QObject
-{
-    Q_OBJECT
+class ExampleService : public QObject {
+  Q_OBJECT
 public:
-    explicit ExampleService(QObject *parent = nullptr);
-    Q_INVOKABLE int getOne();
-    // virtual ~ExampleService();
+  explicit ExampleService(QObject *parent = nullptr);
+  Q_INVOKABLE int getOne();
+  // virtual ~ExampleService();
 
 signals:
-    void sendUnsolicitedNotification(const QString&, const QVariant&);
-
+  void sendUnsolicitedNotification(const QString &, const QVariant &);
 };
 
 #endif // EXAMPLESERVICE_H
